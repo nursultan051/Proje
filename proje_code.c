a43799b87cd6acfd68733ca3eb00e2c51fc378f5
Mintesnot G. Horsa <23181616068@gazi.edu.tr>
2024-12-20 13:24:08 +0300

    Update proje_code.c
    baslama_dakika ve bitis_dakika sabit oldugu icin directive olarak define ettik

--- proje_code.c
+++ proje_code.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include<string.h>
 
+#define baslama_dakika (9 * 60)  //Başlama saati (09:00) dakikayla
+#define bitis_dakika (17 * 60)   //Bitiş saatı (17:00) dakikayla
 
 int main(){
     //Çalışanın verilerini tutmak için yapi tanımladık.
@@ -22,16 +24,20 @@ int main(){
         printf("dosya acma basarisiz!\n");
         return 1;
     }  
-    int max_gec_sicil=0;  //En GEÇ GELEN çalışanın sicili
-    int max_erken_sicil=0;  //En ERKEN GİDEN çalışanın sicili
-    int max_gec_dakika=0;  //En geç gelenin kaç dakıka geciktiği
-    int max_erken_dakika=0;  //En erken gidenin kaç dakika erken gittiği 
-    int baslama_dakika=9*60; //Başlama saati (09:00) dakikayla
-    int bitis_dakika=17*60;  //Bitiş saatı (17:00) dakikayla
-    while(fscanf(gunluk,"%d %d:%d %c", &calisan.sicil, &calisan.saat, &calisan.dakika, &calisan.girisCikis)!=EOF)
+    int max_gec_sicil = 0;  //En GEÇ GELEN çalışanın sicili
+    int max_erken_sicil = 0;  //En ERKEN GİDEN çalışanın sicili
+    int max_gec_dakika = 0;  //En geç gelenin kaç dakıka geciktiği
+    int max_erken_dakika = 0;  //En erken gidenin kaç dakika erken gittiği 
+
+    
+    /* 'gunluk' dosyasından çalışan verilerini (sicil numarası, saat, dakika ve giriş/çıkış durumu) 
+    dosyanın sonuna (EOF) ulaşılana kadar okur.*/
+
+    while(fscanf(gunluk,"%d %d:%d %c", &calisan.sicil, &calisan.saat, &calisan.dakika, &calisan.girisCikis) != EOF)
     {
-        int toplam_dakika= (calisan.saat*60)+calisan.dakika;
-            //Gecikme dakikalarını hesaplar ve en çok gecikeni bulur
+        int toplam_dakika= (calisan.saat * 60) + calisan.dakika;
+        
+        //Gecikme dakikalarını hesaplar ve en çok gecikeni bulur
         if(calisan.girisCikis=='<' && toplam_dakika > baslama_dakika)
         {
           int gecikme= toplam_dakika - baslama_dakika;
@@ -41,24 +47,26 @@ int main(){
               max_gec_sicil=calisan.sicil;
            }
         }
-            //Kaç dakika erken gittiğini hesaplar ve en erken gideni bulur
-        if(calisan.girisCikis=='>' && toplam_dakika < bitis_dakika)
+        
+        //Kaç dakika erken gittiğini hesaplar ve en erken gideni bulur
+        if(calisan.girisCikis == '>' && toplam_dakika < bitis_dakika)
         {
-          int erken_cikis= bitis_dakika - toplam_dakika;
-          if(erken_cikis>max_erken_dakika)
+          int erken_cikis = bitis_dakika - toplam_dakika;
+          if(erken_cikis > max_erken_dakika)
            {
-             max_erken_dakika=erken_cikis;
-             max_erken_sicil=calisan.sicil;
+             max_erken_dakika = erken_cikis;
+             max_erken_sicil = calisan.sicil;
            }
         }
     }
+    
      /*En gec gelenin ve En erken gidenin sicil numaralarını
       ve kaç dakikaya gecikmiş ve erken gitmiş olduklarını dosyalara yazdırır */
-    if(max_gec_sicil!=0)
+    if(max_gec_sicil != 0)
     {
         fprintf(gec, "%d %d\n", max_gec_sicil, max_gec_dakika);
     }
-    if(max_erken_sicil!=0)
+    if(max_erken_sicil != 0)
     {
         fprintf(erken, "%d %d\n", max_erken_sicil, max_erken_dakika);
     }
